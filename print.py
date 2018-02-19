c34e522de3463c45bb096ec420205bed533d3c5b
Paul Seyfert <pseyfert@cern.ch>
2018-02-19 13:15:58 +0100

    clean up tz juggeling and fall back to isoformat
    (first part of the reply here
    https://stackoverflow.com/questions/10944047/remove-pytz-timezone/10944136#10944136)

--- print.py
+++ print.py
@@ -2,11 +2,6 @@ import subprocess
 import datetime
 import pytz
 
-# microseconds are okay, but time zone isn't
-# TODO: use isoformat after adjusting time zone (i.e. remove timezone from
-#       datetime object again
-# TODO: look up the time zone rather than assuming standard cern time zone
-
 print("% ^* <"
       + subprocess.check_output(["git",
                                  "config",
@@ -16,7 +11,8 @@ print("% ^* <"
                                 )[:-1]
       + "> "
       + datetime.datetime.now(tz=pytz.utc)
-        .strftime("%Y-%m-%dT%H:%m:%S.%f")
+        .replace(tzinfo=None)
+        .isoformat()
       + "Z:")
 print("%")
 print("% WRITEME")
