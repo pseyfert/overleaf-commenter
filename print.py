a6b9ae89d8446e15c7d890f992ac12541836c883
Paul Seyfert <pseyfert@cern.ch>
2018-02-19 12:39:13 +0100

    https://julien.danjou.info/blog/2015/python-and-timezones

--- print.py
+++ print.py
@@ -1,6 +1,6 @@
 import subprocess
 import datetime
-from pytz import timezone
+import pytz
 
 # microseconds are okay, but time zone isn't
 # TODO: use isoformat after adjusting time zone (i.e. remove timezone from
@@ -15,9 +15,7 @@ print("% ^* <"
                                  "user.email"]
                                 )[:-1]
       + "> "
-      + timezone('Europe/Zurich')
-        .localize(datetime.datetime.now())
-        .astimezone(timezone('UTC'))
+      + datetime.datetime.now(tz=pytz.utc)
         .strftime("%Y-%m-%dT%H:%m:%S.%f")
       + "Z:")
 print("%")
