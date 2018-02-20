d55a7aaf23cc73b42bd9672f911574da571f4149
Paul Seyfert <pseyfert@cern.ch>
2018-02-20 12:54:29 +0100

    simple answer detection

--- vim-overleaf.py
+++ vim-overleaf.py
@@ -1,5 +1,4 @@
 # create an overleaf comment in the line below the curser
-# TODO: correctly answer to comment (replace current '% ^.' line and start with '^')
 # TODO: current output puts »b'email-address@server.tld'« instead of »email-address@server.tld« (python3)
 
 import subprocess
@@ -7,25 +6,45 @@ import datetime
 import pytz
 import vim
 
-mylines = [
-           "% ^* <{}> {}Z:".format(
-                                   subprocess.check_output(["git",
-                                                            "config",
-                                                            "--get",
-                                                            "user.email"]
-                                                           )[:-1],
-                                   datetime
-                                   .datetime
-                                   .now(tz=pytz.utc)
-                                   .replace(tzinfo=None)
-                                   .isoformat()
-                                  ),
-           "%",
-           "% WRITEME",
-           "%",
-           "% ^.",
-]
+with open('/tmp/overleaf-debug.txt', 'w') as f:
+    currentline = int(vim.eval('line(".")'))
+    encoding = vim.eval("&encoding")
+    currenttext = vim.current.buffer[currentline-1]
+    replymode = currenttext[:4] == "% ^."
+    f.write(currenttext)
+    f.write('\n')
+    if replymode:
+        f.write("in reply mode\n")
+    else:
+        f.write("Not in reply mode\n")
 
-currentline = int(vim.eval('line(".")'))
+    if replymode:
+        leader = "% ^"
+    else:
+        leader = "% *"
 
-vim.current.buffer[currentline:currentline] = mylines
+    mylines = [
+               "{} <{}> {}Z:".format(leader,
+                                     subprocess.check_output(["git",
+                                                              "config",
+                                                              "--get",
+                                                              "user.email"]
+                                                             )[:-1],
+                                     datetime
+                                     .datetime
+                                     .now(tz=pytz.utc)
+                                     .replace(tzinfo=None)
+                                     .isoformat()
+                                     ),
+               "%",
+               "% WRITEME",
+               "%",
+               "% ^.",
+    ]
+
+    if replymode:
+        vim.current.buffer[currentline-1:currentline] = mylines
+    else:
+        vim.current.buffer[currentline:currentline] = mylines
+
+    vim.eval('cursor({},3)'.format(currentline+3))
